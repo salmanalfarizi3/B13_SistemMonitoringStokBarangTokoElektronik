54bb472834bd89c9fbff6f679144a47735f91f8b
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-04-15 05:30:13 +0700

    Set txtID to ReadOnly during selection to protect Primary Key

--- SI Monotoring Stok Barang Pada TOKO ELEKTRONIK/Form1.cs
+++ SI Monotoring Stok Barang Pada TOKO ELEKTRONIK/Form1.cs
@@ -185,5 +185,5 @@ namespace SI_Monotoring_Stok_Barang_Pada_TOKO_ELEKTRONIK
                     txtKat.Text = row.Cells[2].Value.ToString();
                     txtHarga.Text = row.Cells[3].Value.ToString();
                     txtStok.Text = row.Cells[4].Value.ToString();
-                   
-}
+                    txtID.ReadOnly = true;
+               
