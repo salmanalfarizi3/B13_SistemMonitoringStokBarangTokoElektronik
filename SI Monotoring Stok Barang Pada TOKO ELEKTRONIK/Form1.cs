3d1e5a8e7896293490e0acf4d445d2609d1e0c38
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-04-15 05:22:07 +0700

    Add user notification for switching to history view mode

--- SI Monotoring Stok Barang Pada TOKO ELEKTRONIK/Form1.cs
+++ SI Monotoring Stok Barang Pada TOKO ELEKTRONIK/Form1.cs
@@ -160,4 +160,6 @@ namespace SI_Monotoring_Stok_Barang_Pada_TOKO_ELEKTRONIK
                     DataTable dt = new DataTable();
                     da.Fill(dt);
                     dataGridView1.DataSource = dt;
-                   
+                    MessageBox.Show("Menampilkan Riwayat (Log) Transaksi");
+                }
+         
